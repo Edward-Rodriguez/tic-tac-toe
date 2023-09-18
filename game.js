55bbf9897097397c41d7749452cc33e5726e44fd
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2023-09-18 15:03:56 -0400

    Add gameController module

--- game.js
+++ game.js
@@ -16,7 +16,7 @@ const gameBoard = (() => {
     if (availableCells.length === 0 || board[row][col].getValue() !== null)
       return;
     else {
-      board[row][col].addToken(player);
+      board[row][col].addToken(player.getToken());
     }
   };
 
@@ -25,7 +25,6 @@ const gameBoard = (() => {
       row.map((cell) => cell.getValue())
     );
     console.log(boardwithCells);
-    console.log('available cells:');
   };
 
   return { getBoard, placeToken, printBoard };
@@ -50,4 +49,61 @@ function Player(name, token) {
   return { getName, getToken };
 }
 
-gameBoard.placeToken(0, 0, 'x');
+const gameController = (() => {
+  const playerOne = Player('Player One', 'X');
+  const playerTwo = Player('Player Two', '0');
+  const activePlayer = playerOne;
+  const getActivePlayer = () => activePlayer;
+  const switchPlayerTurn = () =>
+    activePlayer === playerOne ? playerTwo : playerOne;
+  const playRound = (row, col) => {
+    gameBoard.placeToken(row, col, activePlayer);
+
+    const playerHasWon = () => {
+      gameBoard.printBoard();
+      let board = gameBoard.getBoard();
+      let columnFlag = false;
+
+      // when columnFlag = true, check winning cols instead
+      for (let row = 0; row < 3; row++) {
+        let winningLine = true;
+        for (let col = 0; col < 3; col++) {
+          let cell = columnFlag ? board[col][row] : board[row][col];
+          if (cell.getValue() !== activePlayer.getToken()) {
+            winningLine = false;
+            break;
+          }
+        }
+        if (winningLine) return true;
+        // check winning columns instead
+        if (row === 2 && columnFlag === false) {
+          columnFlag = true;
+          row = -1;
+        }
+      }
+
+      //check diaganol lines
+      return [
+        board[0][0].getValue(),
+        board[1][1].getValue(),
+        board[2][2].getValue(),
+      ].every((cell) => cell === activePlayer.getToken()) ||
+        [
+          board[2][0].getValue(),
+          board[1][1].getValue(),
+          board[0][2].getValue(),
+        ].every((cell) => cell === activePlayer.getToken())
+        ? true
+        : false;
+    };
+    playerHasWon()
+      ? console.log(activePlayer.getName, 'is the winner')
+      : console.log(`no winner yet, other players turn`);
+  };
+
+  return { getActivePlayer, playRound };
+})();
+
+gameController.playRound(0, 2);
+gameController.playRound(1, 2);
+gameController.playRound(2, 2);
